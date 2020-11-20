672080c52149f2fa1581d355fa7b907d5805410e
manguz14 <66106191+manguz14@users.noreply.github.com>
2020-11-19 18:26:35 -0600

    Update L4.py

--- L4.py
+++ L4.py
@@ -7,9 +7,8 @@ from scipy import stats
 import matplotlib.pyplot as plt
 
 # Variables aleatorias A y Z
-vaX=  stats.norm(0, np.sqrt(10))
-vaY = stats.norm(0, np.sqrt(10))
-
+vaX=  stats.norm(0, np.sqrt(5))
+vaY = stats.norm(0, np.sqrt(5))
 
 # Creación del vector de tiempo
 T = 100			# número de elementos
@@ -18,19 +17,19 @@ t = np.linspace(0, t_final, T)
 
 # Inicialización del proceso aleatorio X(t) con N realizaciones
 N = 10
-W_t = np.empty((N, len(t)))	# N funciones del tiempo x(t) con T puntos
+W = np.empty((N, len(t)))	# N funciones del tiempo x(t) con T puntos
 
 # Creación de las muestras del proceso x(t) (A y Z independientes)
 
 for i in range(N):   
    X = vaX.rvs()        
    Y = vaY.rvs()     
-   w_t = X*np.cos(np.pi*t) + Y*np.sin(np.pi*t) 
-   W_t[i,:] = w_t
-   plt.plot(t, w_t)
+   w = X*np.cos(np.pi*t) + Y*np.sin(np.pi*t) 
+   W[i,:] = w
+   plt.plot(t, w)
 
 # Promedio de las N realizaciones en cada instante (cada punto en t)
-P = [np.mean(W_t[:,i]) for i in range(len(t))]
+P = [np.mean(W[:,i]) for i in range(len(t))]
 plt.plot(t, P, lw=6)
 
 # Graficar el resultado teórico del valor esperado
@@ -58,11 +57,11 @@ plt.figure()
 # Cálculo de correlación para cada valor de tau
 for n in range(N):
 	for i, tau in enumerate(desplazamiento):
-		corr[n, i] = np.correlate(W_t[n,:], np.roll(W_t[n,:], tau))/T
+		corr[n, i] = np.correlate(W[n,:], np.roll(W[n,:], tau))/T
 	plt.plot(taus, corr[n,:])
 
 # Valor teórico de correlación
-Rww = 100*np.cos(np.pi*taus)
+Rww = 25*np.cos(np.pi*taus)
 
 # Gráficas de correlación para cada realización 
 plt.plot(taus, Rww, '-.', lw=4, label='Correlación teórica')
